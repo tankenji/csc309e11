24b8234dd5954a5d9b8f612458a3b7db4045a3fe
tankenji <kenji.tan@mail.utoronto.ca>
2024-11-28 13:52:08 -0500

    added csc309-e11.tsx

---
+++ pages/csc309-e11.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div id="auto-tester">
+        Hi. I am tanken1, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>
+    );
+  }
